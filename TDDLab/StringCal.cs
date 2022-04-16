5d3ef0dee3cb4db94b45b96509f16b126e16394f
Luis Bazaldua <LEBazaldua@student.fullsail.edu>
2022-04-16 15:47:04 -0400

    Add project files.

---
+++ TDDLab/StringCal.cs
@@ -0,0 +1,37 @@
+﻿namespace TDDLab
+{
+    public class StringCal
+    {
+        private string numString;
+        private char dlimtr = ',';
+
+
+        public StringCal()
+        {
+            numString = "";
+
+        }
+
+        public StringCal(string _NumsString)
+        {
+            numString = _NumsString;
+
+
+        }
+
+        public char Delim
+        {
+            get { return dlimtr; }
+            set { dlimtr = value; }
+        }
+
+
+        public void Add()
+        {
+
+
+
+        }
+
+    }
+}
---
+++ TDDLabTests/StringCalTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDLab.Tests
+{
+    [TestClass()]
+    public class StringCalTests
+    {
+        [TestMethod()]
+        public void StringCalTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
